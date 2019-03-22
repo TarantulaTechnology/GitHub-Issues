7845d06c124c908575da132ce33156e916a0dc1c
Mark Morris <TarantulaTechnology@users.noreply.github.com>
2019-03-21 21:03:55 -0500

    README.md updated

--- src/main/java/mm/issues/Main.java
+++ src/main/java/mm/issues/Main.java
@@ -8,29 +8,29 @@ package mm.issues;
  * 
  * GitHub Issues <br>
  *<br>
- * Create a program that generates a report about the the Issues belonging to a
+ * Generates a report about the the Issues belonging to a
  * list of github repositories ordered by creation time, and information about
  * the day when most Issues were created.<br>
  *<br>
  * Input: List of 1 to n Strings with Github repositories references with
  * the format "owner/repository"<br>
- *<br>
+ *<br> 
  *
  * Output: String representation of a Json dictionary with the following
  * content:<br>
  *<br>
  * - "issues": List containing all the Issues related to all the repositories
- * provided. The list should be ordered by the Issue "created_at" field (From
- * oldest to newest) Each entry of the list will be a dictionary with basic
+ * provided. The list is ordered by the Issue "created_at" field (From
+ * oldest to newest) Each entry of the list is a dictionary with basic
  * Issue information: "id", "state", "title", "repository" and "created_at"
  * fields. Issue entry example: { "id": 1, "state": "open", "title": "Found a
  * bug", "repository": "owner1/repository1", "created_at":
  * "2011-04-22T13:33:48Z" }
  *<br><br>
  * - "top_day": Dictionary with the information of the day when most Issues were
- * created. It will contain the day and the number of Issues that were created
- * on each repository this day If there are more than one "top_day", the latest
- * one should be used. example: { "day": "2011-04-22", "occurrences": {
+ * created. Contains the day and the number of Issues that were created
+ * on each repository this day. If there are more than one "top_day", the latest
+ * one is used. example: { "day": "2011-04-22", "occurrences": {
  * "owner1/repository1": 8, "owner2/repository2": 0, "owner3/repository3": 2 } }
  *<br><br>
  *
@@ -47,12 +47,6 @@ package mm.issues;
  * "owner2/repository2": 0 } } }<br>
  *<br>
  * <br>
- *<br>
- * You can create the classes and methods you consider. You can use any library
- * you need. Good modularization, error control and code style will be taken
- * into account. Memory usage and execution time will be taken into account.
- *<br>
- * Good Luck!<br>
  * 
  * @author markmorris
  * @version 1.0
